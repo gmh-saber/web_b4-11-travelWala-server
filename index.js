5d9c40dc21bac6ec577c2165f347f2dc50b61b62
Golam Muktadir Hossain Saber <63154732+gmh-saber@users.noreply.github.com>
2023-07-20 12:57:51 +0600

    Update index.js

--- index.js
+++ index.js
@@ -11,8 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 
-
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qrgom.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qrgom.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
